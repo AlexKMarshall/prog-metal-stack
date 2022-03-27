0cb6694d3e7301ec7421b130c1bad5a33190a753
Alex Marshall <alex.k.marshall83@gmail.com>
2022-03-27 10:17:27 +0100

    chore: remove fly related things

--- remix.init/index.js
+++ remix.init/index.js
@@ -1,72 +1,50 @@
-const { execSync } = require("child_process");
-const crypto = require("crypto");
-const fs = require("fs/promises");
-const path = require("path");
-const inquirer = require("inquirer");
+const { execSync } = require('child_process')
+const crypto = require('crypto')
+const fs = require('fs/promises')
+const path = require('path')
+const inquirer = require('inquirer')
 
-const toml = require("@iarna/toml");
-const sort = require("sort-package-json");
-
-function escapeRegExp(string) {
-  // $& means the whole matched string
-  return string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
-}
+const sort = require('sort-package-json')
 
 function getRandomString(length) {
-  return crypto.randomBytes(length).toString("hex");
+  return crypto.randomBytes(length).toString('hex')
 }
 
 async function main({ rootDirectory }) {
-  const README_PATH = path.join(rootDirectory, "README.md");
-  const FLY_TOML_PATH = path.join(rootDirectory, "fly.toml");
-  const EXAMPLE_ENV_PATH = path.join(rootDirectory, ".env.example");
-  const ENV_PATH = path.join(rootDirectory, ".env");
-  const PACKAGE_JSON_PATH = path.join(rootDirectory, "package.json");
+  const EXAMPLE_ENV_PATH = path.join(rootDirectory, '.env.example')
+  const ENV_PATH = path.join(rootDirectory, '.env')
+  const PACKAGE_JSON_PATH = path.join(rootDirectory, 'package.json')
 
-  const REPLACER = "indie-stack-template";
+  const DIR_NAME = path.basename(rootDirectory)
+  const SUFFIX = getRandomString(2)
 
-  const DIR_NAME = path.basename(rootDirectory);
-  const SUFFIX = getRandomString(2);
-
-  const APP_NAME = (DIR_NAME + "-" + SUFFIX)
+  const APP_NAME = (DIR_NAME + '-' + SUFFIX)
     // get rid of anything that's not allowed in an app name
-    .replace(/[^a-zA-Z0-9-_]/g, "-");
+    .replace(/[^a-zA-Z0-9-_]/g, '-')
 
-  const [prodContent, readme, env, packageJson] = await Promise.all([
-    fs.readFile(FLY_TOML_PATH, "utf-8"),
-    fs.readFile(README_PATH, "utf-8"),
-    fs.readFile(EXAMPLE_ENV_PATH, "utf-8"),
-    fs.readFile(PACKAGE_JSON_PATH, "utf-8"),
-  ]);
+  const [, env, packageJson] = await Promise.all([
+    fs.readFile(EXAMPLE_ENV_PATH, 'utf-8'),
+    fs.readFile(PACKAGE_JSON_PATH, 'utf-8'),
+  ])
 
   const newEnv = env.replace(
     /^SESSION_SECRET=.*$/m,
     `SESSION_SECRET="${getRandomString(16)}"`
-  );
-
-  const prodToml = toml.parse(prodContent);
-  prodToml.app = prodToml.app.replace(REPLACER, APP_NAME);
-
-  const newReadme = readme.replace(
-    new RegExp(escapeRegExp(REPLACER), "g"),
-    APP_NAME
-  );
+  )
 
   const newPackageJson =
     JSON.stringify(
       sort({ ...JSON.parse(packageJson), name: APP_NAME }),
       null,
       2
-    ) + "\n";
+    ) + '\n'
 
   await Promise.all([
-    fs.writeFile(FLY_TOML_PATH, toml.stringify(prodToml)),
-    fs.writeFile(README_PATH, newReadme),
     fs.writeFile(ENV_PATH, newEnv),
     fs.writeFile(PACKAGE_JSON_PATH, newPackageJson),
-  ]);
+  ])
 
-  execSync(`npm run setup`, { stdio: "inherit", cwd: rootDirectory });
+  execSync(`npm run setup`, { stdio: 'inherit', cwd: rootDirectory })
 
   // TODO: There is currently an issue with the test cleanup script that results
   // in an error when running Cypress in some cases. Add this question back
@@ -84,26 +62,26 @@ async function main({ rootDirectory }) {
 
 Start development with \`npm run dev\`
     `.trim()
-  );
+  )
 }
 
 async function askSetupQuestions({ rootDirectory }) {
   const answers = await inquirer.prompt([
     {
-      name: "validate",
-      type: "confirm",
+      name: 'validate',
+      type: 'confirm',
       default: false,
       message:
-        "Do you want to run the build/tests/etc to verify things are setup properly?",
+        'Do you want to run the build/tests/etc to verify things are setup properly?',
     },
-  ]);
+  ])
 
   if (answers.validate) {
     console.log(
       `Running the validate script to make sure everything was set up properly`
-    );
-    execSync(`npm run validate`, { stdio: "inherit", cwd: rootDirectory });
+    )
+    execSync(`npm run validate`, { stdio: 'inherit', cwd: rootDirectory })
   }
 }
 
-module.exports = main;
+module.exports = main
